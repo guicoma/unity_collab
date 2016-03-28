91c2d73e24552b91461a7babe2379d66b0419415
comius <guillemcoma@gmail.com>
2016-03-28 03:10:50 +0200

    Unity-project

---
+++ Assets/Scripts/PlayerControllerJS.js
@@ -0,0 +1,19 @@
+﻿#pragma strict
+
+var speed : float;
+private var rb : Rigidbody;
+
+function Start () {
+	rb = GetComponent.<Rigidbody>();
+}
+
+function Update () {
+
+}
+
+function FixedUpdate () {
+	var moveHorizontal = Input.GetAxis ("Horizontal");
+	var moveVertical = Input.GetAxis ("Vertical");
+	var movement : Vector3 = new Vector3(moveHorizontal, 0.0f, moveVertical);
+	rb.AddForce(movement * speed);
+}
